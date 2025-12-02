3f43b4df9f5f9ad75334a6f1fe6cf6d8c6513344
jun <pickjog@gmail.com>
2025-12-02 14:26:20 +0900

    first commit

---
+++ src/test/java/com/tickatch/product_service/ProjectInterfaceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.tickatch.product_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectInterfaceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
