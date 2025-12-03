c7cfb323d6d5aac87ed7e30f2cb85baae4de5028
jun <pickjog@gmail.com>
2025-12-03 18:42:24 +0900

    feat: 상품 검색 요청 dto 생성

---
+++ src/main/java/com/tickatch/product_service/product/presentation/api/dto/ProductSearchRequest.java
@@ -0,0 +1,21 @@
+package com.tickatch.product_service.product.presentation.api.dto;
+
+import com.tickatch.product_service.product.domain.repository.dto.ProductSearchCondition;
+import com.tickatch.product_service.product.domain.vo.ProductStatus;
+import com.tickatch.product_service.product.domain.vo.ProductType;
+
+public record ProductSearchRequest(
+    String name,
+    ProductType productType,
+    ProductStatus status,
+    Long stageId
+) {
+  public ProductSearchCondition toCondition() {
+    return ProductSearchCondition.builder()
+        .name(name)
+        .productType(productType)
+        .status(status)
+        .stageId(stageId)
+        .build();
+  }
+}
