53b7f7dfe5c9862f2d16f54c3902c90af9e49024
jun <pickjog@gmail.com>
2025-12-03 18:41:55 +0900

    feat: 상품 스테이지 변경 dto 생성

---
+++ src/main/java/com/tickatch/product_service/product/presentation/api/dto/StageChangeRequest.java
@@ -0,0 +1,9 @@
+package com.tickatch.product_service.product.presentation.api.dto;
+
+import jakarta.validation.constraints.NotNull;
+
+public record StageChangeRequest(
+    @NotNull(message = "스테이지 ID는 필수입니다")
+    Long stageId
+) {
+}
